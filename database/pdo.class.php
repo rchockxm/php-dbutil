3e45f56173264c1e25a3a721713cc2adc036f0a5
Rchockxm <rchockxm.silver@gmail.com>
2014-11-15 16:30:43 +0800

    Create pdo.class.php

---
+++ database/pdo.class.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * DB-Util
+ * Created by Silence Unlimited
+ * Developer: Rchockxm (rchockxm.silver@gmail.com)
+ * FileName: pdo.class.php
+ */
+namespace DBCls\Database;
+
+/**
+ * Database DBPDO
+ */
+class DBPDO extends \DBCls\DB implements \DBCls\IADatabase { 
+
+    /**
+     * __construct()
+     * @type public
+     * @args void
+     * @return Success - void
+     *         Fail - void
+     */
+    public function __construct() { }
+    
+    /**
+     * __desctuct()
+     * @type public
+     * @args void
+     * @return Success - void
+     *         Fail - void
+     */
+    public function __destruct() { }
+}
